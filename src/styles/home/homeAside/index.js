8b5c83a17a710644114e2d699e092a57791a2ecd
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-07-17 19:38:34 +0900

    [추가] : style 초기화. layout 및 home component style 추가

---
+++ src/styles/home/homeAside/index.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const HomeAsideStyle = styled.div``;
+
+export default HomeAsideStyle;
---
+++ src/styles/home/homeMain/index.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const HomeSectionStyle = styled.div``;
+
+export default HomeSectionStyle;
