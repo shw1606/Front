66e2a278d806c1e276342b09dff6740be64eec79
JangJua <jua0610@naver.com>
2020-07-22 17:20:01 +0900

    [수정]: Router가 최상단에 오도록 수정

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "App.jsx";
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+ReactDOM.render((
+    <BrowserRouter>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    </BrowserRouter>
+    ), document.getElementById("root")
 );
