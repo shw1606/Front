def77f58ae7d50734780de3c57345eae6fbbdbfd
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-07-27 15:19:02 +0900

    [변경] : 트랜딩 게시글, 최신 게시글 불러오기 리듀서 변경

--- src/store/reducers/postReducer.js
+++ src/store/reducers/postReducer.js
@@ -6,24 +6,50 @@ import {
   POSTS_LOAD_SUCCESS,
   POSTS_LOAD_FAILURE,
 } from "store/actions/postAction";
+// 최신 게시글 load action
+import {
+  RECENT_POSTS_LOAD_REQUEST,
+  RECENT_POSTS_LOAD_SUCCESS,
+  RECENT_POSTS_LOAD_FAILURE,
+} from "store/actions/postAction";
 
 const initialState = {
   posts: [],
+  recentPosts: [],
   hasMorePosts: true,
+  hasMoreRecentPosts: true,
+  showPostFallback: false,
 };
 
 const PostReducer = (state = initialState, action) => {
   return immer(state, (draft) => {
     switch (action.type) {
       case POSTS_LOAD_REQUEST: {
+        draft.showPostFallback = false;
         break;
       }
       case POSTS_LOAD_SUCCESS: {
         draft.hasMorePosts = action.data.length === 20;
         draft.posts.push(...action.data);
+        draft.showPostFallback = true;
         break;
       }
       case POSTS_LOAD_FAILURE: {
+        draft.showPostFallback = false;
+        break;
+      }
+      case RECENT_POSTS_LOAD_REQUEST: {
+        draft.showPostFallback = false;
+        break;
+      }
+      case RECENT_POSTS_LOAD_SUCCESS: {
+        draft.hasMoreRecentPosts = action.data.length === 20;
+        draft.recentPosts.push(...action.data);
+        draft.showPostFallback = true;
+        break;
+      }
+      case RECENT_POSTS_LOAD_FAILURE: {
+        draft.showPostFallback = false;
         break;
       }
       default: {
