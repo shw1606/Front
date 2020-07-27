565546fe4bf9c5205cbd165e43feb6f84c7f477e
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-07-27 15:20:22 +0900

    [변경] : HomePostLayout 내용 변경(url에 따라서 트랜딩 게시글, 최신 게시글 띄우도록 변경)

--- src/components/Home/HomePostLayout/HomePost/index.jsx
+++ src/components/Home/HomePostLayout/HomePost/index.jsx
@@ -5,34 +5,36 @@ import { BsFillHeartFill } from "react-icons/bs";
 // styles
 import { HomePostStyle } from "components/Home/HomePostLayout/HomePost/style";
 import { PostImage } from "components/Home/HomePostLayout/HomePost/style";
-import { PostDescription } from "components/Home/HomePostLayout/HomePost/style";
-import { PostSubInfo } from "components/Home/HomePostLayout/HomePost/style";
+import { MainInfo } from "components/Home/HomePostLayout/HomePost/style";
+import { Payload } from "components/Home/HomePostLayout/HomePost/style";
+import { Date } from "components/Home/HomePostLayout/HomePost/style";
+import { SubInfo } from "components/Home/HomePostLayout/HomePost/style";
 
 const HomePost = ({ data }) => {
   return (
     <>
       <HomePostStyle>
-        <PostImage to="/">
+        <PostImage to={`/@${data.author}/${data.title}`}>
           <div>
             <img src={data.thumnail} />
           </div>
         </PostImage>
-        <PostDescription>
-          <Link to="/">
+        <MainInfo>
+          <Payload to={`/@${data.author}/${data.title}`}>
             <h4>{data.title}</h4>
             <div>
               <p>{data.content}</p>
             </div>
-          </Link>
-          <div>
+          </Payload>
+          <Date>
             <span>게시일자</span>
             <span className="separator"></span>
             <span>조회수</span>
             <span>수정일자</span>
-          </div>
-        </PostDescription>
-        <PostSubInfo>
-          <Link to="/">
+          </Date>
+        </MainInfo>
+        <SubInfo>
+          <Link to={`/@${data.author}/${data.title}`}>
             <img src={data.avatar} />
             <span>
               by <b>{data.author}</b>
@@ -42,7 +44,7 @@ const HomePost = ({ data }) => {
             <BsFillHeartFill />
             {data.heart}
           </div>
-        </PostSubInfo>
+        </SubInfo>
       </HomePostStyle>
     </>
   );
--- src/components/Home/HomePostLayout/HomePost/style.js
+++ src/components/Home/HomePostLayout/HomePost/style.js
@@ -44,41 +44,57 @@ export const PostImage = styled(Link)`
   }
 `;
 
-export const PostDescription = styled.div`
+export const MainInfo = styled.div`
   display: flex;
   flex-direction: column;
   padding: 1rem;
   flex: 1 1 0%;
-  a {
-    display: block;
-    color: inherit;
-    text-decoration: none;
-    h4 {
-      font-size: 1rem;
-      line-height: 1.5;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      overflow-x: hidden;
-      overflow-y: hidden;
-      color: rgb(33, 37, 41);
-      margin: 0px 0px 0.25rem;
-      @media (max-width: 767px) {
-        white-space: initial;
-      }
-    }
-  } /* a */
-  div {
-    font-size: 0.75rem;
+`;
+
+export const Payload = styled(Link)`
+  display: block;
+  color: inherit;
+  text-decoration: none;
+  h4 {
+    font-size: 1rem;
     line-height: 1.5;
-    color: rgb(134, 142, 150);
-    .separator {
-      margin-left: 0.25rem;
-      margin-right: 0.25rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    color: rgb(33, 37, 41);
+    margin: 0px 0px 0.25rem;
+    @media (max-width: 767px) {
+      white-space: initial;
     }
+  }
+  p {
+    word-break: break-word;
+    overflow-wrap: break-word;
+    font-size: 0.875rem;
+    line-height: 1.5;
+    height: 3.9375rem;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    text-overflow: ellipsis;
+    color: rgb(73, 80, 87);
+    margin: 0px 0px 1.5rem;
+    overflow: hidden;
   }
 `;
 
-export const PostSubInfo = styled.div`
+export const Date = styled.div`
+  font-size: 0.75rem;
+  line-height: 1.5;
+  color: rgb(134, 142, 150);
+  .separator {
+    margin-left: 0.25rem;
+    margin-right: 0.25rem;
+  }
+`;
+
+export const SubInfo = styled.div`
   display: flex;
   font-size: 0.75rem;
   line-height: 1.5;
