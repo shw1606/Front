4c60fef4888d825a6ef04a2331c9b0f2ae87febd
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-07-27 15:21:04 +0900

    [추가] : 게시글 더미 데이터 추가(더미 데이터는 이후 삭제 예정)

--- src/saga/postSaga.js
+++ src/saga/postSaga.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { all, call, delay, fork, put, takeLatest } from "redux-saga/effects";
 
-// import postDummy1 from "saga/dummy.json";
-// import postDummy2 from "saga/aditional1.json";
-// import postDummy3 from "saga/aditional2.json";
-// import postDummy4 from "saga/aditional3.json";
+import postDummy1 from "saga/dummy.json";
+import postDummy2 from "saga/aditional1.json";
+import postDummy3 from "saga/aditional2.json";
+import postDummy4 from "saga/aditional3.json";
 
 // load post actions
 import {
@@ -12,9 +12,15 @@ import {
   POSTS_LOAD_SUCCESS,
   POSTS_LOAD_FAILURE,
 } from "store/actions/postAction";
+// 최신 게시글 load action
+import {
+  RECENT_POSTS_LOAD_REQUEST,
+  RECENT_POSTS_LOAD_SUCCESS,
+  RECENT_POSTS_LOAD_FAILURE,
+} from "store/actions/postAction";
 
 function loadPostsApi({ id = 0, heart = 0 }) {
-  // return id === 0 ? postDummy1 : postDummy2;
+  return id === 0 ? postDummy1 : postDummy2;
   // return axios.get(`/postIoad?id=${id}&heart=${heart}`)
 }
 
@@ -38,6 +44,31 @@ function* watchLoadPosts() {
   yield takeLatest(POSTS_LOAD_REQUEST, loadPosts);
 }
 
+function loadRecentPostsApi() {
+  return postDummy3;
+  // return axios.get(`/postIoad?id=${id}&heart=${heart}`)
+}
+
+function* loadRecentPosts(action) {
+  try {
+    yield delay(1000);
+    const result = yield call(loadRecentPostsApi);
+    yield put({
+      type: RECENT_POSTS_LOAD_SUCCESS,
+      data: result.data,
+    });
+  } catch (error) {
+    yield put({
+      type: RECENT_POSTS_LOAD_FAILURE,
+      data: error,
+    });
+  }
+}
+
+function* watchLoadRecentPosts() {
+  yield takeLatest(RECENT_POSTS_LOAD_REQUEST, loadRecentPosts);
+}
+
 export default function* () {
-  yield all([fork(watchLoadPosts)]);
+  yield all([fork(watchLoadPosts), fork(watchLoadRecentPosts)]);
 }
