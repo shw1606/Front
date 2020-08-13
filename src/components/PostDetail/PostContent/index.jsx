7869f527708a6cfd14475b0ea4cfd2722b1eb00c
JangJua <jua0610@naver.com>
2020-08-14 03:30:51 +0900

    [수정]: markdown 스타일링 개선

--- src/components/PostDetail/PostContent/index.jsx
+++ src/components/PostDetail/PostContent/index.jsx
@@ -1,28 +1,114 @@
 import React from "react";
 import {useSelector} from "react-redux";
 import ReactMarkdown from "react-markdown";
-import CodeBlock from "./CodeBlock";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 const PostContent = () => {
-  const post = useSelector((state) => state.postDetailReducer.postInfo);
-
+  const content = useSelector((state) => state.postDetailReducer.postInfo.context);
   return (
-    <ReactMarkdown source={post.context}
+    <ReactMarkdown source={content}
+                   skipHtml={false}
+                   escapeHtml={false}
                    renderers={{
                      blockquote: BlockQuoteBlock,
                      tableCell: TableCellBlock,
-                     code: CodeBlock
-                   }}
-    />
+                     code: CodeBlock,
+                     inlineCode: InlineCodeBlock,
+                     root: Root,
+                     link: Link,
+                     linkReference: Link,
+                     image: Image,
+                     imageReference: Image,
+                     listItem: ListItem,
+                     thematicBreak: ThematicBreak,
+                     paragraph: Paragraph
+                   }}/>
   );
+
+  function ThematicBreak() {
+    return (
+      <hr style={{
+        border: '1px solid #dedede',
+        margin: '2em 0'
+      }}/>
+    )
+  }
+
+  function Paragraph(props) {
+    return (
+      <div>
+        {props.children}
+      </div>
+    );
+  }
+
+  function ListItem(props) {
+    return (
+      <li style={{
+        padding: '0.2em 0',
+        lineHeight: '1.6em'
+      }}>
+        {props.children}
+      </li>
+    )
+  }
+
+  function Image(props) {
+    return (
+      <img src={props.src} style={{
+        maxWidth: '100%'
+      }}/>
+    )
+  }
+
+  function Root(props) {
+    return (
+      <div style={{
+        whiteSpace: 'pre',
+        padding: '0.2em 0'
+      }}>
+        {props.children}
+      </div>
+    )
+  }
+
+  function Link(props) {
+    return (
+      <div style={{
+        color: '#0ca678'
+      }}>
+        {props.children}
+      </div>
+    )
+  }
+
+  function CodeBlock(props) {
+    const { language, value } = props;
+    return (
+      <SyntaxHighlighter language={language}>
+        {value}
+      </SyntaxHighlighter>
+    );
+  }
+
+  function InlineCodeBlock(props) {
+    const { value } = props;
+    return (
+      <span style={{
+        backgroundColor: '#f5f2f0',
+        padding: '0.2em 0.4em',
+      }}>
+        {value}
+      </span>
+    );
+  }
 
   function BlockQuoteBlock(props) {
     return (
       <div style={{
-        color: '#666',
         margin: 0,
         paddingLeft: '2em',
-        borderLeft: '0.3em solid #eee'
+        borderLeft: '0.5em solid #12b886'
       }}>
         {props.children}
       </div>
@@ -31,14 +117,16 @@ const PostContent = () => {
 
   function TableCellBlock(props) {
     let style = {
+      color: '#343a40',
       textAlign : props.align ? props.align : 'center',
-      border: '1.5px solid #eee',
+      border: '0.7px solid #343a40',
       margin: 0,
       padding: 5
     };
 
     if(props.isHeader) {
-      style.fontWeight = 'bold'
+      style.fontWeight = 'bold';
+      style.borderBottom = '2px solid #343a40';
     }
 
     return (
