46fac76918f650939bf86826fbd3565aa6626a54
JangJua <jua0610@naver.com>
2020-08-23 17:35:43 +0900

    [변경]: thumbnail로 변수명 변경

---
+++ src/components/User/UserPostList/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './style';
+import dummy from 'saga/userPostListDummy.json';
+
+function UserPostList() {
+  return (
+    <S.UserPostListWrapper>
+      <div> UserPosts </div>
+    </S.UserPostListWrapper>
+  );
+}
+
+export default UserPostList;
--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -6,6 +6,7 @@ import { Switch, Route, useParams } from "react-router-dom";
 import UserTabs from "components/User/UserTabs";
 import UserSeries from "../../components/User/UserSeries";
 import UserAbout from "../../components/User/UserAbout";
+import UserPostList from "../../components/User/UserPostList";
 
 const User = () => {
   const { tab } = useParams();
@@ -16,6 +17,7 @@ const User = () => {
       <UserProfile id="test123"/>
       <UserTabs type={tab} username={username} />
       <Switch>
+        <Route path={`/profile/@${username}`} exact component={UserPostList} />
         <Route path={`/profile/@${username}/series`} component={UserSeries} />
         <Route path={`/profile/@${username}/about`} component={UserAbout} />
       </Switch>
