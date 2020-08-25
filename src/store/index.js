1ac8fbd5521f16365f1c029856ec67dc99347cc3
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-08-26 01:04:27 +0900

    [변경] : modal 및 register redux, redux-saga 추가되어 file 내용 변경

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,14 @@ import common from "store/reducers/commonReducer";
 import postReducer from "store/reducers/postReducer";
 import postDetailReducer from "store/reducers/postDetailReducer";
 import userReducer from "store/reducers/userReducer";
+import register from "store/reducers/registerReducer";
+import modal from "store/reducers/modalReducer";
 
 export default combineReducers({
   common,
   postReducer,
   postDetailReducer,
   userReducer,
+  register,
+  modal,
 });
