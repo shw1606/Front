23a4846130ff98994aa561b1c89a12472356e603
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-09-13 23:29:12 +0900

    [추가] : writeReducer 추가

---
+++ src/store/actions/writeAction.js
@@ -0,0 +1,60 @@
+// 글쓰기 창에서 입력한 내용을 markdown으로 변환
+export const CHANGE_MARKDOWN = "CHANGE_MARKDOWN";
+export const changeMarkdown = (data) => ({
+  type: CHANGE_MARKDOWN,
+  data,
+});
+
+// 제목 내용을 markdown으로 변환
+export const CHANGE_TITLE = "CHANGE_TITLE";
+export const changeTitle = (data) => ({
+  type: CHANGE_TITLE,
+  data,
+});
+
+// 마크다운 이미지 업로드
+export const UPLOAD_MARKDOWN_IMAGE_REQUEST = "UPLOAD_MARKDOWN_IMAGE_REQUEST";
+export const UPLOAD_MARKDOWN_IMAGE_SUCCESS = "UPLOAD_MARKDOWN_IMAGE_SUCCESS";
+export const UPLOAD_MARKDOWN_IMAGE_FAILURE = "UPLOAD_MARKDOWN_IMAGE_FAILURE";
+export const uploadMarkdownImageRequest = (data) => ({
+  type: UPLOAD_MARKDOWN_IMAGE_REQUEST,
+  data,
+});
+
+// 글쓰기 세팅 화면 출력여부 설정
+export const PRINT_WRITE_SETTING_LAYOUT = "PRINT_WRITE_SETTING_LAYOUT";
+export const printWriteSettingLayout = (data) => ({
+  type: PRINT_WRITE_SETTING_LAYOUT,
+  data,
+});
+
+// 썸네일 업로드
+export const UPLOAD_THUMBNAIL_REQUEST = "UPLOAD_THUMBNAIL_REQUEST";
+export const UPLOAD_THUMBNAIL_SUCCESS = "UPLOAD_THUMBNAIL_SUCCESS";
+export const UPLOAD_THUMBNAIL_FAILURE = "UPLOAD_THUMBNAIL_FAILURE";
+export const uploadThumbnailRequest = (data) => ({
+  type: UPLOAD_THUMBNAIL_REQUEST,
+  data,
+});
+
+// 업로드 된 썸네일 제거
+export const REMOVE_THUMBNAIL = "REMOVE_THUMBNAIL";
+export const removeThumbnail = () => ({ type: REMOVE_THUMBNAIL });
+
+// 시리즈 리스트 불러오기
+export const LOAD_SERIES_LIST_REQUEST = "LOAD_SERIES_LIST_REQUEST";
+export const LOAD_SERIES_LIST_SUCCESS = "LOAD_SERIES_LIST_SUCCESS";
+export const LOAD_SERIES_LIST_FAILURE = "LOAD_SERIES_LIST_FAILURE";
+export const loadSeriesList = (data) => ({
+  type: LOAD_SERIES_LIST_REQUEST,
+  data,
+});
+
+// 시리즈 input값 전달
+export const SUBMIT_SERIES_INPUT_REQUEST = "SUBMIT_SERIES_INPUT_REQUEST";
+export const SUBMIT_SERIES_INPUT_SUCCESS = "SUBMIT_SERIES_INPUT_SUCCESS";
+export const SUBMIT_SERIES_INPUT_FAILURE = "SUBMIT_SERIES_INPUT_FAILURE";
+export const submitSeriesInput = (data) => ({
+  type: SUBMIT_SERIES_INPUT_REQUEST,
+  data,
+});
---
+++ src/store/reducers/writeReducer/index.js
@@ -0,0 +1,104 @@
+import immer from "immer";
+
+// actions
+import { CHANGE_MARKDOWN } from "store/actions/writeAction";
+import { CHANGE_TITLE } from "store/actions/writeAction";
+import { PRINT_WRITE_SETTING_LAYOUT } from "store/actions/writeAction";
+import { LOAD_SERIES_LIST_SUCCESS } from "store/actions/writeAction";
+import { UPLOAD_THUMBNAIL_SUCCESS } from "store/actions/writeAction";
+import { REMOVE_THUMBNAIL } from "store/actions/writeAction";
+import {
+  UPLOAD_MARKDOWN_IMAGE_REQUEST,
+  UPLOAD_MARKDOWN_IMAGE_SUCCESS,
+} from "store/actions/writeAction";
+import {
+  SUBMIT_SERIES_INPUT_REQUEST,
+  SUBMIT_SERIES_INPUT_SUCCESS,
+  SUBMIT_SERIES_INPUT_FAILURE,
+} from "store/actions/writeAction";
+
+// case
+import changeMarkdown from "./changeMarkdown";
+import changeTitle from "./changeTitle";
+import printWriteSetting from "./printWriteSetting";
+import uploadThumbnail from "./uploadThumbnail";
+import removeThumbnail from "./removeThumbnail";
+import * as uploadMarkdownImage from "./uploadMarkdownImage";
+import * as submitSeriesInput from "./submitSeriesInput";
+import * as loadSeries from "./loadSeries";
+
+const initialState = {
+  title: "",
+  markdown: "",
+  markdownImage: "",
+  printWriteSetting: false, // 글쓰기 세팅 화면 출력여부 설정
+  seriesList: [],
+  thumbnail: null,
+};
+
+const RegisterReducer = (state = initialState, action) => {
+  return immer(state, (draft) => {
+    switch (action.type) {
+      // title 변환
+      case CHANGE_TITLE: {
+        changeTitle(draft, action.data);
+        break;
+      }
+      // markdown 변환
+      case CHANGE_MARKDOWN: {
+        changeMarkdown(draft, action.data);
+        break;
+      }
+      // 마크다운 이미지 업로드 요청
+      case UPLOAD_MARKDOWN_IMAGE_REQUEST: {
+        uploadMarkdownImage.request(draft, "");
+        break;
+      }
+      // 마크다운 이미지 업로드 성공
+      case UPLOAD_MARKDOWN_IMAGE_SUCCESS: {
+        uploadMarkdownImage.success(draft, action.data);
+        break;
+      }
+      // 글쓰기 세팅 화면 출력여부 설정
+      case PRINT_WRITE_SETTING_LAYOUT: {
+        printWriteSetting(draft, action.data);
+        break;
+      }
+      // 썸네일 업로드
+      case UPLOAD_THUMBNAIL_SUCCESS: {
+        uploadThumbnail(draft, action.data);
+        break;
+      }
+      // 업로드 된 썸네일 제거
+      case REMOVE_THUMBNAIL: {
+        removeThumbnail(draft, action.data);
+        break;
+      }
+      // 시리즈 리스트 불러오기
+      case LOAD_SERIES_LIST_SUCCESS: {
+        loadSeries.success(draft, action.data);
+        break;
+      }
+      // 시리즈 인풋값 전달 요청
+      case SUBMIT_SERIES_INPUT_REQUEST: {
+        submitSeriesInput.request(draft, action.data);
+        break;
+      }
+      // 시리즈 인풋값 전달 성공
+      case SUBMIT_SERIES_INPUT_SUCCESS: {
+        submitSeriesInput.suceess(draft, action.data);
+        break;
+      }
+      // 시리즈 인풋값 전달 실패
+      case SUBMIT_SERIES_INPUT_FAILURE: {
+        submitSeriesInput.failure(draft, action.data);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  });
+};
+
+export default RegisterReducer;
