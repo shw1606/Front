36e105473a2d505c5e3b7d87875567d4eed34eb5
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-09-13 23:35:52 +0900

    [변경] : 임시 글 라우팅 링크 적용

--- src/components/Header/UserButton/DropdownMenu.jsx
+++ src/components/Header/UserButton/DropdownMenu.jsx
@@ -27,7 +27,7 @@ function DropdownMenu({ username }) {
       {dropdownVisible && (
         <DropdownContent ref={contentRef}>
           <StyledLink to={`/profile/@${username}`}> 내 벨로그 </StyledLink>
-          <StyledLink to="/"> 임시 글 </StyledLink>
+          <StyledLink to="/saves"> 임시 글 </StyledLink>
           <StyledLink to="/"> 읽기 목록 </StyledLink>
           <StyledLink to="/"> 설정 </StyledLink>
           <StyledLink to="/"> 로그아웃 </StyledLink>
