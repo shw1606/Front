cbb5d6fac58eb3a5790fc434b5aded065d6fd1f3
JangJua <jua0610@naver.com>
2020-09-15 02:34:24 +0900

    [수정]: 본문 width 설정, component 위치 변경

--- src/pages/PostDetail/style.js
+++ src/pages/PostDetail/style.js
@@ -1,15 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 0 4rem 0 4rem;
-  margin-top: 5.5rem;
-
+  width: 65rem;
+  margin: 0 auto;
   @media (max-width: 1024px) {
     margin-top: 2rem;
+    width: 100%;
     padding: 0 2rem 0 2rem;
   }
 
   @media (max-width: 768px) {
     padding: 0 0.5rem 0 0.5rem;
   }
+`;
+
+export const Wrapper = styled.div`
+  margin: 5.5rem auto;
+
 `;
