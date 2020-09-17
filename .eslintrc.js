23120e7de5cc54ba904c233ae5b673e53857c032
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-09-17 17:32:56 +0900

    [추가] : eslint 라이브러리 추가

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+    allowImportExportEverywhere: true,
+  },
+  plugins: ["react"],
+  rules: {
+    "linebreak-style": 0,
+    "import/no-unresolved": 0,
+    "import/no-extraneous-dependencies": 0,
+    "import/prefer-default-export": 0,
+    "implicit-arrow-linebreak": 0,
+    "operator-linebreak": 0,
+    "comma-dangle": 0,
+    "object-curly-newline": 0,
+    "import/no-duplicates": 0,
+    quotes: 0,
+  },
+};
