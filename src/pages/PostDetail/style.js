639fbb87200d45a7a530ac859d5be061c461f167
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-09-28 18:10:44 +0900

    [수정] : 게시글 상세 페이지 너비 수정

--- src/pages/PostDetail/style.js
+++ src/pages/PostDetail/style.js
@@ -1,20 +1,18 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 65rem;
+  width: 768px;
   margin: 0 auto;
   @media (max-width: 1024px) {
     margin-top: 2rem;
-    width: 100%;
     padding: 0 2rem 0 2rem;
   }
-
   @media (max-width: 768px) {
+    width: 100%;
     padding: 0 0.5rem 0 0.5rem;
   }
 `;
 
 export const Wrapper = styled.div`
   margin: 5.5rem auto;
-
 `;
